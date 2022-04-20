f6b717f92b5105e4b49211aff80d574b8a65fcbd
Daniel Kuffner <daniel.kuffner@avid.com>
2022-04-20 17:01:34 +0200

    fix resource name

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {  
     cloudux-utils = {
       source  = "AvidCloud/cloudux-utils"
-      version = ">= 0.0.3"
+      version = ">= 0.0.5"
     }
   }
 }
@@ -16,9 +16,9 @@ resource "cloudux-utils_site_key" "example" {
 }
 
 output "public_key" {
-  value = site_key.example.public_key
+  value = cloudux-utils_site_key.example.public_key
 }
 
 output "private_key" {
-  value = site_key.example.private_key
+  value = cloudux-utils_site_key.example.private_key
 }
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -14,7 +14,7 @@ func Provider() *schema.Provider {
 		Schema: map[string]*schema.Schema{},
 
 		ResourcesMap: map[string]*schema.Resource{
-			"site_key": resourceSiteKey(),
+			"cloudux-utils-site_key": resourceSiteKey(),
 		},
 		DataSourcesMap:       map[string]*schema.Resource{},
 		ConfigureContextFunc: providerConfigure,
